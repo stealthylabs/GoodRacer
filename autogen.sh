136bb79ab464b5dcc5544653b91ffdad49b5bc93
Stealthy Labs <github@stealthylabs.com>
2020-05-12 23:59:12 -0400

    Adding dependencies as submodules

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+CURDIR=`readlink -f $(dirname $0)`
 libtoolize --copy
 aclocal && autoheader && automake --add-missing && autoconf
+cd $CURDIR/libgps_mtk3339 && ./autogen.sh && cd $CURDIR
+cd $CURDIR/libssd1306 && ./autogen.sh && cd $CURDIR
+
